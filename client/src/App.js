c8429506c247fad804f782ad3a305381bad08a93
SimonChen <ringosimonchen0820@gmail.com>
2020-01-29 20:19:50 -0500

    MVP

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import {Route, Switch} from 'react-router-dom';
 import SavedList from './Movies/SavedList';
 import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
+
 
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
@@ -12,16 +14,17 @@ const App = () => {
 
   return (
     <div>
-      <SavedList list={savedList} />
-      <div>
-        <Switch>
-          <Route exact path='/'>
-            <MovieList />
-          </Route>
-        </Switch>
-      </div>
+      <Route path='/' render={props => <SavedList {...props} list={savedList} />} />
+      {/* <SavedList list={savedList} {props => ...props}/> */}
+      <Switch>
+        <Route exact path='/'><MovieList /></Route>
+        {/* <Route path='/movies/:id' component={Movie} /> */}
+        <Route path='/movies/:id' render={props => <Movie {...props} addToSavedList={addToSavedList}/>}/>
+
+      </Switch>
     </div>
   );
 };
 
 export default App;
+
